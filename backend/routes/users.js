5e104221af6e7d7985aaf5d461f36dc452b4ab6d
Alice Fang <alicesf2@illinois.edu>
2020-04-25 00:25:27 -0500

    get and post users works

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,1 +1,32 @@
+const { User } = require("../models");
 
+module.exports = function(router) {
+  const usersRoute = router.route("/users");
+  const userRoute = router.route("/users/:id");
+
+  // get all Users
+  usersRoute.get(async (req, res) => {
+    const users = await User.find({});
+    res.status(200).send({
+      message: "Successfully retrieved all users.",
+      data: users
+    });
+  });
+
+  // create a User
+  usersRoute.post(async (req, res) => {
+    const { name, email, password, school, admin } = req.body;
+    const newUser = new User({
+      name,
+      email,
+      password,
+      school,
+      admin
+    });
+    await newUser.save();
+    res.status(200).send({
+      message: "Successfully created new user."
+    });
+  });
+  return router;
+};
