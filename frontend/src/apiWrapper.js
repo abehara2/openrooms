864941871d7c1f7838ca98ab158d47bfec56a2f8
Alice Fang <alicesf2@illinois.edu>
2020-04-26 12:41:16 -0500

    react app backend url

--- frontend/src/apiWrapper.js
+++ frontend/src/apiWrapper.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const BASE_URL = process.env.BACKEND_URL ?? "http://localhost:4000/api";
+const BASE_URL =
+  process.env.REACT_APP_BACKEND_URL ?? "http://localhost:4000/api";
 
 export const getUserByID = User_ID => {
   const requestString = `${BASE_URL}/users/${User_ID}`;
